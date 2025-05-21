946cb719a18452e20c5879ce5c9bda944bbba50f
LaysC <laiscarvalho858@gmail.com>
2025-05-21 16:07:59 -0300

    JS com chamada à API e criação dos cards dos Pokémons

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('pokemon-container');
+
+async function getPokemon(id) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const data = await response.json();
+  return data;
+}
+
+function createPokemonCard(pokemon) {
+  const card = document.createElement('div');
+  card.classList.add('pokemon-card');
+  card.innerHTML = `
+    <h3>${pokemon.name}</h3>
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    <p>Tipo: ${pokemon.types.map(t => t.type.name).join(', ')}</p>
+  `;
+  container.appendChild(card);
+}
+
+async function loadPokemons() {
+  for (let i = 1; i <= 10; i++) {
+    const pokemon = await getPokemon(i);
+    createPokemonCard(pokemon);
+  }
+}
+
+loadPokemons();
